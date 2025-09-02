bdbcbae275d8f5b46f66c837dbab00fb3fe8d33c
Logan Boehringer <boehringerl17@gmail.com>
2025-09-02 17:11:11 -0400

    Program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Logan Boehringer
+ *        Date: 9/02/2025
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
